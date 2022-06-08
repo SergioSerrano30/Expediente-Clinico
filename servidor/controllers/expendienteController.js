73043a01ad583eed4f4342eb39e74ab8ef1f3122
SergioSerrano30 <84033208+SergioSerrano30@users.noreply.github.com>
2022-06-07 23:16:29 -0600

    .

--- servidor/controllers/expendienteController.js
+++ servidor/controllers/expendienteController.js
@@ -1,5 +1,6 @@
 const req = require("express/lib/request");
 const res = require("express/lib/response");
+//const Expediente = requiere('../models/Expediente.js');
 
 exports.crearExpediente = (req, res) => {
     console.log('Desde crear expediente')
